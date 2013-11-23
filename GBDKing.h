ef193dd1f56c369754e30947b5fb98f06761bd96
homsar47 <nicklausw47@gmail.com>
2013-11-23 14:31:41 -0800

    Create GBDKing.h

---
+++ GBDKing.h
@@ -0,0 +1,20 @@
+/*=========================================
+==HOMSAR47'S GDDKing PROJECT===============
+==DESIGNED TO ADD BETTER FUNCTIONALITY=====
+TO GBDK, A PROGRAM MADE TO PROGRAM=========
+GAME BOY GAMES IN C========================
+=========================================*/
+
+/*==BELOW ARE ALL THE BUTTONS.==========
+====WHEN WRITING JOYPAD() TO AN=========
+====INTEGER, THESE ARE THE GIVEN========
+====VALUES.===========================*/
+
+#define I_A       16
+#define I_B       32
+#define I_UP      4
+#define I_DOWN    8
+#define I_LEFT    2
+#define I_RIGHT   1
+#define I_START   -128
+#define I_SELECT  64
